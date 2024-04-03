c62f97457745384d0ce230fcae018159edf7b96a
luckydev <sundayluckyenyinna@gmail.com>
2024-04-03 08:36:50 +0100

    Added module for swagger documentation

--- src/main/java/com/accionmfb/omnix/core/docs/SwaggerDocProperties.java
+++ src/main/java/com/accionmfb/omnix/core/docs/SwaggerDocProperties.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class SwaggerDocProperties {
     private String email = "noreply@accionmfb.com";
     private String name = "Micro - Service";
-    private String url = "";
-    private String title = "";
+    private String url = "https://www.swagger-docs.com";
+    private String title = "Micro - Service API";
     private String version = "1.0";
-    private String description = "";
+    private String description = "Central documentation for micro-service api";
 }
--- src/main/java/com/accionmfb/omnix/core/localsource/LocalCacheSourceModule.java
+++ src/main/java/com/accionmfb/omnix/core/localsource/LocalCacheSourceModule.java
@@ -3,16 +3,14 @@ package com.accionmfb.omnix.core.localsource;
 import com.accionmfb.omnix.core.localsource.config.SimpleLocalCacheConfigurer;
 import com.accionmfb.omnix.core.localsource.core.impl.SimpleLocalParamCache;
 import com.accionmfb.omnix.core.localsource.core.impl.SimpleLocalParamCacheFallback;
-import com.accionmfb.omnix.core.localsource.http.LocalCacheUpdateController;
-import com.accionmfb.omnix.core.localsource.http.LocalCacheUpdateService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        SimpleLocalCacheConfigurer.class, SimpleLocalParamCache.class,
-        SimpleLocalParamCacheFallback.class, LocalCacheUpdateController.class,
-        LocalCacheUpdateService.class
+        SimpleLocalCacheConfigurer.class,
+        SimpleLocalParamCache.class,
+        SimpleLocalParamCacheFallback.class,
 })
 public class LocalCacheSourceModule {
 }
